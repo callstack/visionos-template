415f17b439aa3a10aef19f72d1521fc38d6d9510
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2025-01-23 17:11:23 +0100

    feat: add RCTAppDependencyProvider()

--- template/visionos/HelloWorld/AppDelegate.swift
+++ template/visionos/HelloWorld/AppDelegate.swift
@@ -1,8 +1,15 @@
 import UIKit
 import React
 import React_RCTAppDelegate
+import ReactAppDependencyProvider
 
 class AppDelegate: RCTAppDelegate {
+  override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    self.dependencyProvider = RCTAppDependencyProvider()
+    
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
   override func sourceURL(for bridge: RCTBridge) -> URL? {
     self.bundleURL()
   }
